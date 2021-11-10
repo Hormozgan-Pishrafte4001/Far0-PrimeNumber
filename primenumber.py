122525cd3fc4462ba78ec5c06b8605691a88caa0
kimiamhzb <kimiya.mhzb@gmail.com>
2021-11-10 14:56:50 +0330

    Add the functionality to isprime() method

--- primenumber.py
+++ primenumber.py
@@ -3,8 +3,14 @@ class PrimeNumber:
     nonpnumber = []
 
     def isprime(self, number):
-        if type(number) != int:
-            raise TypeError('Number is not integer')
+        if not isinstance(number, (int, list)):
+            raise TypeError('Number is not integer nor list')
+        if isinstance(number, list):
+            result = []
+            for num in number:
+                result.append(self.isprime(num))
+            return result
+
         if number in self.nonpnumber:
             return False
         elif number in self.pnumber:
@@ -15,6 +21,12 @@ class PrimeNumber:
             if number % i == 0:
                 self.nonpnumber.append(number)
                 return False
-            
+
         self.pnumber.append(number)
         return True
+
+
+obj = PrimeNumber()
+print(obj.isprime([2, 3, 4, 5, 6, 7]))
+print(obj.isprime(13))
+print(obj.isprime(16))
